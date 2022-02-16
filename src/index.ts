bad0ecf6768bdb87e1506096fdbb4b65445fdb88
RyosukeSomeya <rx.0.0618@gmail.com>
2022-02-16 23:00:22 +0900

    サンプル追加

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
-window.addEventListener('load', (event) => {
-  let p = document.querySelector('#target');
-  p.textContent = 'This is message by TypeScript';
-});
+import { MyData } from './lib';
+
+const mydata = new MyData();
+mydata.add('taro', 39);
+mydata.add('hanako', 28);
+mydata.add('sachiko', 17);
+mydata.add('jiro', 6);
+
+mydata.print();
