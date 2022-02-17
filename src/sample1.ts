f83a6eedf6e9de33df553743366d36706a7236a0
RyosukeSomeya <rx.0.0618@gmail.com>
2022-02-17 23:55:05 +0900

    sample2

--- src/sample1.ts
+++ src/sample1.ts
@@ -1,26 +1,15 @@
 let msg: HTMLParagraphElement;
 
-const html = `<h3>This is message</h3>
-<div id="content">wait...</div>`;
+const html = `<h2>This is message</h2>
+<div id="content">wait</div>`;
 
 window.addEventListener('load', () => {
   msg = document.querySelector('#msg');
   msg.innerHTML = html;
   const content: HTMLDivElement = document.querySelector('#content');
   setDiv(content);
+  addElement(content);
 });
-
-function addElement(event: MouseEvent) {
-  const div: HTMLDivElement = document.createElement('div');
-  div.style.position = 'absolute';
-  div.style.width = '50px';
-  div.style.height = '50px';
-  div.style.left = event.offsetX - 25 + 'px';
-  div.style.top = event.offsetY - 25 + 'px';
-  div.style.backgroundColor = '#cc00aa33';
-  const target: HTMLElement = event.target as HTMLElement;
-  target.appendChild(div);
-}
 
 function setDiv(content: HTMLDivElement) {
   content.style.position = 'absolute';
@@ -33,5 +22,17 @@ function setDiv(content: HTMLDivElement) {
   content.style.borderColor = 'red';
   content.style.backgroundColor = 'white';
   content.textContent = '';
-  content.addEventListener('click', addElement);
+}
+
+function addElement(content: HTMLDivElement) {
+  for (let i = 1; i <= 7; i++) {
+    let div: HTMLDivElement = document.createElement('div');
+    div.style.position = 'absolute';
+    div.style.width = '100px';
+    div.style.height = '100px';
+    div.style.top = i * 25 + 'px';
+    div.style.left = i * 25 + 'px';
+    div.style.backgroundColor = '#aa00cc33';
+    content.appendChild(div);
+  }
 }
