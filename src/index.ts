6cea0b93837f2847911343b0bebaa15122ec91ab
RyosukeSomeya <rx.0.0618@gmail.com>
2022-02-17 23:46:07 +0900

    sample 1

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,38 @@
-import { MyData } from './lib';
+let msg: HTMLParagraphElement;
 
-const mydata = new MyData();
-mydata.add('taro', 39);
-mydata.add('hanako', 28);
-mydata.add('sachiko', 17);
-mydata.add('jiro', 6);
+const html = `<h2>This is message</h2>
+<div id="content">wait</div>`;
 
-mydata.print();
+window.addEventListener('load', () => {
+  msg = document.querySelector('#msg');
+  msg.innerHTML = html;
+  const content: HTMLDivElement = document.querySelector('#content');
+  setDiv(content);
+  addElement(content);
+});
+
+function setDiv(content: HTMLDivElement) {
+  content.style.position = 'absolute';
+  content.style.left = '100px';
+  content.style.top = '100px';
+  content.style.width = '300px';
+  content.style.height = '300px';
+  content.style.borderWidth = '3px';
+  content.style.borderStyle = 'solid';
+  content.style.borderColor = 'red';
+  content.style.backgroundColor = 'white';
+  content.textContent = '';
+}
+
+function addElement(content: HTMLDivElement) {
+  for (let i = 1; i <= 7; i++) {
+    let div: HTMLDivElement = document.createElement('div');
+    div.style.position = 'absolute';
+    div.style.width = '100px';
+    div.style.height = '100px';
+    div.style.top = i * 25 + 'px';
+    div.style.left = i * 25 + 'px';
+    div.style.backgroundColor = '#aa00cc33';
+    content.appendChild(div);
+  }
+}
